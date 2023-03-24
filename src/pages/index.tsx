efa90f44d24434097a1ae086719f1bdf66822dc9
josersn <nettorammos@hotmail.com>
2023-03-24 13:13:19 -0300

    chore: base layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/laura-bday.png" />
       </Head>
+      <audio autoPlay>
+        <source src="musica.mp3" type="audio/mp3" />
+      </audio>
       <main className={styles.main}>
-        
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+          </div>
+
+          <p className={styles.code}>Laura, esse é o mês do seu aniversário e estamos todos aqui com você pra te acompanhar nesse mês.</p>
+
+        </div>
       </main>
     </>
   )
