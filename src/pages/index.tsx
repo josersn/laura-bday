726a9c6571a5bf746787d89737e4ba2638971b02
josersn <nettorammos@hotmail.com>
2023-03-26 16:49:45 -0300

    chore: first day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,23 +17,54 @@ export default function Home() {
       <audio autoPlay>
         <source src="musica.mp3" type="audio/mp3" />
       </audio>
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.item}></div>
-            <div className={styles.item}></div>
-            <div className={styles.item}></div>
-          </div>
-          <div className={styles.row}>
-            <div className={styles.item}></div>
-            <div className={styles.item}></div>
-            <div className={styles.item}></div>
-          </div>
-
-          <p className={styles.code}>Laura, esse é o mês do seu aniversário e estamos todos aqui com você pra te acompanhar nesse mês.</p>
-
+      <div className={styles.bolhas}>
+        <span></span>
+        <label></label>
+        <div></div>
+        <label></label>
+        <span></span>
+        <div></div>
+        <main></main>
+        <span></span>
+        <div></div>
+        <label></label>
+        <main></main>
+        <span></span>
+        <label></label>
+        <div></div>
+        <label></label>
+        <span></span>
+        <main></main>
+        <div></div>
+        <label></label>
+        <span></span>
+        <div></div>
+        <label></label>
+        <main></main>
+        <div></div>
+        <label></label>
+        <span></span>
+        <div></div>
+        <label></label>
+        <label></label>
+        <main></main>
+        <span></span>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <Image className={styles.item} src='/nina.jpeg' alt='Nina' width={200} height={210} />
+          <Image className={styles.item} src='/celestial.jpeg' alt='Celestia' width={200} height={210} />
+          <Image className={styles.item} src='/belo.jpeg' alt='Obelix' width={200} height={210} />
         </div>
-      </main>
+        <div className={styles.row}>
+          <Image className={styles.item} src='/mel.jpeg' alt='Mel' width={200} height={210} />
+          <Image className={styles.item} src='/gato.jpeg' alt='Gato' width={200} height={210} />
+          <Image className={styles.item} src='/stella.jpeg' alt='Stella' width={200} height={210} />
+        </div>
+
+        <p className={styles.code}>Laura, esse é o mês do seu aniversário e estamos todos aqui com você pra te acompanhar nesse mês.🥳🎉🎁🎊</p>
+
+      </div>
     </>
   )
 }
